da04ab08640201ecd82bdbe221d5df05e7d1196e
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-13 21:19:11 +0530

    stack question 7 added

--- stack/7-largest-rectangle-in-histogram.ts
+++ stack/7-largest-rectangle-in-histogram.ts
@@ -1,37 +1,7 @@
-class Stack<Type> {
-  items: Type[];
-  
-  constructor() {
-    this.items = []
-  }
+import Stack from "../data-structures/stack/stack";
 
-  push(value: Type): void {
-    this.items.push(value);
-  }
-
-  pop(): Type {
-    if (this.isEmpty()) {
-      throw new Error("Stack Underflow");
-    }
-
-    return this.items.pop() as Type;
-  }
-
-  peek(): Type {
-    if (this.isEmpty()) {
-      throw new Error("Stack Underflow");
-    }
-
-    return this.items[this.size() - 1];
-  }
-
-  isEmpty(): boolean {
-    return this.size() === 0;
-  }
-
-  size(): number {
-    return this.items.length;
-  }
+function getArea(width: number, height: number) {
+  return width * height;
 }
 
 function largestRectangleArea(heights: number[]): number {
@@ -42,18 +12,16 @@ function largestRectangleArea(heights: number[]): number {
     let pushbackIndex = i;
     while (stack.size() > 0 && stack.peek()[1] > heights[i]){
       const [index, height] = stack.pop();
-      const currArea = (i - index) * height;
+      const currArea = getArea(i - index, height);
       maxArea = Math.max(maxArea, currArea);
       pushbackIndex = index;
     }
     stack.push([pushbackIndex, heights[i]]);
   }
 
-  const stackLen = stack.size();
-  for (let i = 0; i < stackLen; i++) {
+  while (stack.size() > 0) {
     const [index, height] = stack.pop();
-    console.log({ index, height });
-    const currArea = (heights.length - index) * height;
+    const currArea = getArea(heights.length - index, height);
     maxArea = Math.max(maxArea, currArea);
   }
 
