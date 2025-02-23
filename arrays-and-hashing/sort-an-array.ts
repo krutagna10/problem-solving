f5afd8ba32ca5cb12e48a37e1eb687df7b7a5624
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-23 22:09:47 +0530

    add sort-an-array

---
+++ arrays-and-hashing/sort-an-array.ts
@@ -0,0 +1,42 @@
+function merge(nums: number[], low: number, mid: number, high: number): void {
+  let i = low;
+  let j = mid + 1;
+  let k = 0;
+  const combinedNums = new Array(high - low + 1);
+
+  while (i <= mid && j <= high) {
+    if (nums[i] < nums[j]) {
+      combinedNums[k++] = nums[i++];
+    } else {
+      combinedNums[k++] = nums[j++];
+    }
+  }
+
+  while (i <= mid) {
+    combinedNums[k++] = nums[i++];
+  }
+
+  while (j <= high) {
+    combinedNums[k++] = nums[j++];
+  }
+
+  for (let m = low; m <= high; m++) {
+    nums[m] = combinedNums[m - low];
+  }
+}
+
+function mergeSort(nums: number[], low: number, high: number): void {
+  if (low >= high) {
+    return;
+  }
+
+  const mid = Math.floor((low + high) / 2);
+  mergeSort(nums, low, mid);
+  mergeSort(nums, mid + 1, high);
+  merge(nums, low, mid, high);
+}
+
+function sortArray(nums: number[]): number[] {
+  mergeSort(nums, 0, nums.length - 1);
+  return nums;
+}
