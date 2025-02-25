b07114455fa14ec2406e9dff1b429d4a4131ad0d
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-25 19:29:18 +0530

    modify sort-colors

--- arrays-and-hashing/sort-colors.ts
+++ arrays-and-hashing/sort-colors.ts
@@ -1,13 +1,22 @@
-function sortColors(nums: number[]): void {
-  const counts = new Array(3).fill(0);
-  for (const num of nums) {
-    counts[num]++;
-  }
+function swap(nums: number[], index1: number, index2: number): void {
+  [nums[index1], nums[index2]] = [nums[index2], nums[index1]];
+}
 
-  let start = 0;
-  for (let i = 0; i < counts.length; i++) {
-    const newNums = new Array(counts[i]).fill(i);
-    nums.splice(start, counts[i], ...newNums);
-    start += counts[i];
+function sortColors(nums: number[]): void {
+  let index = 0;
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (index <= right) {
+    if (nums[index] === 0) {
+      swap(nums, index, left);
+      left++;
+      index++;
+    } else if (nums[index] === 2) {
+      swap(nums, index, right);
+      right--;
+    } else {
+      index++;
+    }
   }
 }
