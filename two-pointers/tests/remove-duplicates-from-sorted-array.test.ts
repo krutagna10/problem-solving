cbf7a1c9cee12bb611ca183eaf506c12cbadd6d7
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-03-04 22:31:06 +0530

    add two pointers question

---
+++ two-pointers/tests/remove-duplicates-from-sorted-array.test.ts
@@ -0,0 +1,7 @@
+import { test, expect} from "@jest/globals";
+import removeDuplicates from "../src/remove-duplicates-from-sorted-array";
+
+test("should remove all the duplicates from input array and return the length of new array", () => {
+  expect(removeDuplicates([2, 3, 3, 6, 9, 9])).toBe(4);
+  expect(removeDuplicates([2, 2, 2, 11])).toBe(2);
+})
